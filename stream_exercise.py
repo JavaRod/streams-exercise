c39eea5f803ec674e76ca6d3d5fe99331b2d2c9b
Rod Musser <rmusser@yahoo.com>
2020-10-23 10:43:24 -0700

    Added stream parsing code

--- stream_exercise.py
+++ stream_exercise.py
@@ -6,14 +6,14 @@ class StreamProcessor(object):
         1. You initialize an instance with a stream of digits
           (AKA: file-like object, instance of StringIO), and
           store it as an instance variable.
-        
+
           eg: f = io.StringIO("234761640930110349378289194")
               my_stream_processor = MyStreamProcessor(f)
-              
+
         2. You call a `process` method of my_stream_processor.
-        
+
           This method:
-          
+
             1. Reads two digits at a time from the beginning of the stream
             2. Converts the two digits into a number, and adds that number
                to a running total.
@@ -45,10 +45,11 @@ class StreamProcessor(object):
     def __init__(self, stream):
         self._stream = stream
 
+
     def process(self):
         """
         TODO: Implement the `process` method, as described above.
-        
+
         :return: int
         """
 
@@ -57,11 +58,18 @@ class StreamProcessor(object):
         total = 0  # The running total of sums.
 
         # TODO: WRITE CODE HERE:
+        while True:
+          digits = self._stream.read(2)
+          if digits == '' or len(digits) < 2:
+            break
+          number = (int(digits[0]) * 10) + int(digits[1])
+          total = total + number
+          count = count + 1
+          if (total >= 200) or count == 10:
+             break
 
         # Just some example syntax, you can read two digits from the head of the
         # stream using the following code:
         #
         # digits = self._stream.read(2)
-
-
         return count
